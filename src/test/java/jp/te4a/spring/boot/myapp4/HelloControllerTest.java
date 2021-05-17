c45ed322b6f4c76aa990dee3c1fe4fab8f422e95
木村 光希 <180012@jc-21.jp>
2021-05-17 13:15:24 +0900

    最初のコミットだよーん

---
+++ src/test/java/jp/te4a/spring/boot/myapp4/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package jp.te4a.spring.boot.myapp4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+	@Test
+	public void test() {
+		HelloController helloController = new HelloController();
+		String expected = "this is Spring Boot sample.";
+		String actual = helloController.index();
+		
+		assertEquals(expected, actual);
+	}
+
+}
